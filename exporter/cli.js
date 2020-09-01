997efdbfa0fbc2dfe4a3314c72d8cb671241fb56
Viktor Charypar <charypar@gmail.com>
2020-09-01 20:10:17 +0100

    Rewrite the API client to use GraphQL in order to avoid rate limits

--- exporter/cli.js
+++ exporter/cli.js
@@ -2,12 +2,12 @@ const { getIssueEvents } = require("./index.js");
 const { formatFile } = require("./csv.js");
 
 const API_KEY = process.env.API_KEY;
-const OWNER = "redbadger";
+const OWNER = "valloop";
 const REPO = "valloop";
 const EVENT_TYPES = [
-  "converted_note_to_issue",
-  "added_to_project",
-  "moved_columns_in_project",
+  "ADDED_TO_PROJECT_EVENT",
+  "MOVED_COLUMNS_IN_PROJECT_EVENT",
+  "CONVERTED_NOTE_TO_ISSUE_EVENT",
 ];
 
 getIssueEvents(API_KEY, OWNER, REPO, EVENT_TYPES).then((events) => {
